2d3deed30766d881a2ebd5e67995376a788a66da
pooya parsa <pooya@pi0.ir>
2019-08-23 00:00:47 +0430

    better run

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,18 @@
 <template>
   <div>
-    <div v-if="took">Last run took: {{ took }}ms</div>
     <button @click="run">Run</button>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      took: null
-    }
-  },
   methods: {
     run() {
       const s = new Date()
       this.$store.dispatch('pushData')
-      this.took = new Date() - s
+      const took = new Date() - s
+      alert('Run took: ' + took + 'ms')
+      window.location.reload()
     }
   }
 };
